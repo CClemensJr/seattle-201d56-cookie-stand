cf95ca1764c34b9bb3ba2d6960f775a59ea8e442
Charles Clemens <cclemensjr@gmail.com>
2019-04-01 15:20:50 -0700

    Added remaining objects

--- js/app.js
+++ js/app.js
@@ -1,29 +1,24 @@
 'use strict';
 
-//First, create a separate JS object literal (no constructor functions... yet) for each shop location that does the following:
-//SeaTac Airport
-//Seattle Center
-//Capitol Hill
-//Alki
 const hoursOfOperation = ["6 am", "7 am", "8 am", "9 am", "10 am", "11 am", "12 pm", "1 pm", "2 pm", "3 pm", "4 pm", "5 pm", "6 pm", "7 pm", "8 pm"];
 
 let salesByStore = [];
 
+/*********************
+* STORE OBJECTS
+**/
+
 //1st and pike
 let firstAndPike = {
-  //Stores the min/max hourly customers, and the average cookies per customer, in object properties
   minHourlyCustomers: 23,
   maxHourlyCustomers: 65,
   avgCookiesPerSale: 6.3,
   totalCookiesSold: 0,
   cookiesSoldPerHour: [],
   
-  
-  //Calculate and store the simulated amounts of cookies purchased for each hour at each location using average cookies purchased and the random number of customers generated
   hourlyFigures: function() {
     for (let i = 0; i < hoursOfOperation.length; i++)
     {
-      //Uses a method of that object to generate a random number of customers per hour. Objects/Math/random
       let customersPerHour = randomizer(this.minHourlyCustomers, this.maxHourlyCustomers);
       let hourlySales = Math.floor(customersPerHour * this.avgCookiesPerSale)
 
@@ -32,11 +27,88 @@ let firstAndPike = {
     }
   }
 }
-//Store the results for each location in a separate array... perhaps as a property of the object representing that location
-firstAndPike.hourlyFigures();
-salesByStore.push(firstAndPike.cookiesSoldPerHour);
 
-//
+//SeaTac Airport
+let seaTacAirport = {
+  minHourlyCustomers: 3,
+  maxHourlyCustomers: 24,
+  avgCookiesPerSale: 1.2,
+  totalCookiesSold: 0,
+  cookiesSoldPerHour: [],
+
+  hourlyFigures: function() {
+    for (let i = 0; i < hoursOfOperation.length; i++)
+    {
+      let customersPerHour = randomizer(this.minHourlyCustomers, this.maxHourlyCustomers);
+      let hourlySales = Math.floor(customersPerHour * this.avgCookiesPerSale)
+
+      this.cookiesSoldPerHour.push(hourlySales);
+      this.totalCookiesSold += hourlySales;
+    }
+  }
+}
+
+//Seattle Center
+let seattleCenter = {
+  minHourlyCustomers: 11,
+  maxHourlyCustomers: 38,
+  avgCookiesPerSale: 3.7,
+  totalCookiesSold: 0,
+  cookiesSoldPerHour: [],
+
+  hourlyFigures: function() {
+    for (let i = 0; i < hoursOfOperation.length; i++)
+    {
+      let customersPerHour = randomizer(this.minHourlyCustomers, this.maxHourlyCustomers);
+      let hourlySales = Math.floor(customersPerHour * this.avgCookiesPerSale)
+
+      this.cookiesSoldPerHour.push(hourlySales);
+      this.totalCookiesSold += hourlySales;
+    }
+  }
+}
+
+//Capitol Hill
+let capitolHill = {
+  minHourlyCustomers: 20,
+  maxHourlyCustomers: 38,
+  avgCookiesPerSale: 2.3,
+  totalCookiesSold: 0,
+  cookiesSoldPerHour: [],
+  
+  hourlyFigures: function() {
+    for (let i = 0; i < hoursOfOperation.length; i++)
+    {
+      let customersPerHour = randomizer(this.minHourlyCustomers, this.maxHourlyCustomers);
+      let hourlySales = Math.floor(customersPerHour * this.avgCookiesPerSale)
+
+      this.cookiesSoldPerHour.push(hourlySales);
+      this.totalCookiesSold += hourlySales;
+    }
+  }
+}
+
+//Alki
+let alki = {
+  minHourlyCustomers: 2,
+  maxHourlyCustomers: 16,
+  avgCookiesPerSale: 4.6,
+  totalCookiesSold: 0,
+  cookiesSoldPerHour: [],
+
+  hourlyFigures: function() {
+    for (let i = 0; i < hoursOfOperation.length; i++)
+    {
+      let customersPerHour = randomizer(this.minHourlyCustomers, this.maxHourlyCustomers);
+      let hourlySales = Math.floor(customersPerHour * this.avgCookiesPerSale)
+
+      this.cookiesSoldPerHour.push(hourlySales);
+      this.totalCookiesSold += hourlySales;
+    }
+  }
+}
+
+// The randomizer takes a min and max and returns a random integer.
 function randomizer(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -46,8 +118,12 @@ function randomizer(min, max) {
   return randomNumber;
 }
 
+// Main instantiates all of the object literals
 function main() {
   //Store the results for each location in a separate array... perhaps as a property of the object representing that location
   firstAndPike.hourlyFigures();
   salesByStore.push(firstAndPike.cookiesSoldPerHour);
+
+
+
 }
