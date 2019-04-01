c2d5e780f74cc1b40496cf1b8f50364444ecf2fa
Charles Clemens <cclemensjr@gmail.com>
2019-04-01 14:02:59 -0700

    Added hours of operation

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@
 //Seattle Center
 //Capitol Hill
 //Alki
+const hoursOfOperation = ["6 am", "7 am", "8 am", "9 am", "10 am", "11 am", "12 pm", "1 pm", "2 pm", "3 pm", "4 pm", "5 pm", "6 pm", "7 pm", "8 pm"];
 
 //1st and pike
 let firstAndPike = {
@@ -14,11 +15,13 @@ let firstAndPike = {
   avgCookiesPerSale: 6.3,
   
   //Uses a method of that object to generate a random number of customers per hour. Objects/Math/random
-  randomizeCustomersPerHour: () => Math.random() * (maxHourlyCustomers - minHourlyCustomers) + minHourlyCustomers,
+  randomizeCustomersPerHour: () => Math.random() * (this.maxHourlyCustomers - this.minHourlyCustomers) + this.minHourlyCustomers,
   
   //Calculate and store the simulated amounts of cookies purchased for each hour at each location using average cookies purchased and the random number of customers generated
   hourlyFigures: function() {
-    
+    let salesArray =[];
+
+    for
   }
   
   //Store the results for each location in a separate array... perhaps as a property of the object representing that location
