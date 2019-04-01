021cbc9fb67dc1c676a06bf3e34b2a09ed96ddb3
Charles Clemens <cclemensjr@gmail.com>
2019-04-01 14:06:55 -0700

    Added sales array

--- js/app.js
+++ js/app.js
@@ -7,6 +7,8 @@
 //Alki
 const hoursOfOperation = ["6 am", "7 am", "8 am", "9 am", "10 am", "11 am", "12 pm", "1 pm", "2 pm", "3 pm", "4 pm", "5 pm", "6 pm", "7 pm", "8 pm"];
 
+let dailySales = [];
+
 //1st and pike
 let firstAndPike = {
   //Stores the min/max hourly customers, and the average cookies per customer, in object properties
@@ -21,10 +23,16 @@ let firstAndPike = {
   hourlyFigures: function() {
     let salesArray =[];
 
-    for
+    for (let i = 0; i < hoursOfOperation.length; i++)
+    {
+      salesArray.push(avgCookiesPerSale * randomizeCustomersPerHour);
+    }
+
+    return salesArray;
   }
   
   //Store the results for each location in a separate array... perhaps as a property of the object representing that location
+  let dailySales
 }
 
 
