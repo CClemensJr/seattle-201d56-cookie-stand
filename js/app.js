c62c7b7307bfdf7e1ba9089be7776c1bdc498c61
Charles Clemens <cclemensjr@gmail.com>
2019-04-01 13:56:24 -0700

    Added properties for min, max, and avg

--- js/app.js
+++ js/app.js
@@ -8,16 +8,20 @@
 
 //1st and pike
 let firstAndPike = {
+  //Stores the min/max hourly customers, and the average cookies per customer, in object properties
+  minHourlyCustomers: 23,
+  maxHourlyCustomers: 65,
+  avgCookiesPerSale: 6.3,
   
+  //Uses a method of that object to generate a random number of customers per hour. Objects/Math/random
+  randomizeCustomersPerHour: () => Math.random() * (maxHourlyCustomers - minHourlyCustomers) + minHourlyCustomers,
+  
+  //Calculate and store the simulated amounts of cookies purchased for each hour at each location using average cookies purchased and the random number of customers generated
+
+  
+  //Store the results for each location in a separate array... perhaps as a property of the object representing that location
 }
 
-//Stores the min/max hourly customers, and the average cookies per customer, in object properties
-
-//Uses a method of that object to generate a random number of customers per hour. Objects/Math/random
-
-//Calculate and store the simulated amounts of cookies purchased for each hour at each location using average cookies purchased and the random number of customers generated
-
-//Store the results for each location in a separate array... perhaps as a property of the object representing that location
 
 //Display the values of each array as unordered lists in the browser
 
