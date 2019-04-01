17365c89c1ce3d1e7d737e16418a7d940ddb4be4
Charles Clemens <cclemensjr@gmail.com>
2019-04-01 15:28:30 -0700

    Added array of locations

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const hoursOfOperation = ["6 am", "7 am", "8 am", "9 am", "10 am", "11 am", "12 pm", "1 pm", "2 pm", "3 pm", "4 pm", "5 pm", "6 pm", "7 pm", "8 pm"];
+const locations = ["First and Pike", "SeaTac Airport", "Seattle Center", "Capitol Hill", "Alki"];
 
 let salesByStore = [];
 
@@ -124,6 +125,22 @@ function main() {
   firstAndPike.hourlyFigures();
   salesByStore.push(firstAndPike.cookiesSoldPerHour);
 
+  seaTacAirport.hourlyFigures();
+  salesByStore.push(seaTacAirport.cookiesSoldPerHour);
 
+  seattleCenter.hourlyFigures();
+  salesByStore.push(seattleCenter.cookiesSoldPerHour);
 
+  capitolHill.hourlyFigures();
+  salesByStore.push(capitolHill.cookiesSoldPerHour);
+
+  alki.hourlyFigures();
+  salesByStore.push(alki.cookiesSoldPerHour);
+}
+
+main();
+
+for (let i = 0; i < salesByStore[0].length; i++)
+{
+  h1
 }
