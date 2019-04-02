95923244a03c18cf00be495eb8c23cef704055ee
Charles Clemens <cclemensjr@gmail.com>
2019-04-02 14:00:14 -0700

    Deleted object literals

--- js/app.js
+++ js/app.js
@@ -66,99 +66,6 @@ function createStores() {
   }
 }
 
-//1st and pike
-// let firstAndPike = {
-//   minHourlyCustomers: 23,
-//   maxHourlyCustomers: 65,
-//   avgCookiesPerSale: 6.3,
-//   totalCookiesSold: 0,
-//   cookiesSoldPerHour: [],
-  
-//   hourlyFigures: function() {
-    
-//   }
-// }
-
-//SeaTac Airport
-// let seaTacAirport = {
-//   minHourlyCustomers: 3,
-//   maxHourlyCustomers: 24,
-//   avgCookiesPerSale: 1.2,
-//   totalCookiesSold: 0,
-//   cookiesSoldPerHour: [],
-
-//   hourlyFigures: function() {
-//     for (let i = 0; i < hoursOfOperation.length; i++)
-//     {
-//       let customersPerHour = randomizer(this.minHourlyCustomers, this.maxHourlyCustomers);
-//       let hourlySales = Math.floor(customersPerHour * this.avgCookiesPerSale)
-
-//       this.cookiesSoldPerHour.push(hourlySales);
-//       this.totalCookiesSold += hourlySales;
-//     }
-//   }
-// }
-
-//Seattle Center
-// let seattleCenter = {
-//   minHourlyCustomers: 11,
-//   maxHourlyCustomers: 38,
-//   avgCookiesPerSale: 3.7,
-//   totalCookiesSold: 0,
-//   cookiesSoldPerHour: [],
-
-//   hourlyFigures: function() {
-//     for (let i = 0; i < hoursOfOperation.length; i++)
-//     {
-//       let customersPerHour = randomizer(this.minHourlyCustomers, this.maxHourlyCustomers);
-//       let hourlySales = Math.floor(customersPerHour * this.avgCookiesPerSale)
-
-//       this.cookiesSoldPerHour.push(hourlySales);
-//       this.totalCookiesSold += hourlySales;
-//     }
-//   }
-// }
-
-//Capitol Hill
-// let capitolHill = {
-//   minHourlyCustomers: 20,
-//   maxHourlyCustomers: 38,
-//   avgCookiesPerSale: 2.3,
-//   totalCookiesSold: 0,
-//   cookiesSoldPerHour: [],
-  
-//   hourlyFigures: function() {
-//     for (let i = 0; i < hoursOfOperation.length; i++)
-//     {
-//       let customersPerHour = randomizer(this.minHourlyCustomers, this.maxHourlyCustomers);
-//       let hourlySales = Math.floor(customersPerHour * this.avgCookiesPerSale)
-
-//       this.cookiesSoldPerHour.push(hourlySales);
-//       this.totalCookiesSold += hourlySales;
-//     }
-//   }
-// }
-
-//Alki
-// let alki = {
-//   minHourlyCustomers: 2,
-//   maxHourlyCustomers: 16,
-//   avgCookiesPerSale: 4.6,
-//   totalCookiesSold: 0,
-//   cookiesSoldPerHour: [],
-
-//   hourlyFigures: function() {
-//     for (let i = 0; i < hoursOfOperation.length; i++)
-//     {
-//       let customersPerHour = randomizer(this.minHourlyCustomers, this.maxHourlyCustomers);
-//       let hourlySales = Math.floor(customersPerHour * this.avgCookiesPerSale)
-
-//       this.cookiesSoldPerHour.push(hourlySales);
-//       this.totalCookiesSold += hourlySales;
-//     }
-//   }
-// }
-
 // The randomizer takes a min and max and returns a random integer. Borrowed from MDN.
 function randomizer(min, max) {
   min = Math.ceil(min);
@@ -169,29 +76,6 @@ function randomizer(min, max) {
   return randomNumber;
 }
 
-// Main instantiates all of the object literals
-// function main() {
-//   //Store the results for each location in a separate array... perhaps as a property of the object representing that location
-//   firstAndPike.hourlyFigures();
-//   salesByStore.push(firstAndPike.cookiesSoldPerHour);
-//   allStores.push(firstAndPike);
-
-//   seaTacAirport.hourlyFigures();
-//   salesByStore.push(seaTacAirport.cookiesSoldPerHour);
-//   allStores.push(seaTacAirport)
-
-//   seattleCenter.hourlyFigures();
-//   salesByStore.push(seattleCenter.cookiesSoldPerHour);
-//   allStores.push(seattleCenter)
-
-//   capitolHill.hourlyFigures();
-//   salesByStore.push(capitolHill.cookiesSoldPerHour);
-//   allStores.push(capitolHill)
-
-//   alki.hourlyFigures();
-//   salesByStore.push(alki.cookiesSoldPerHour);
-//   allStores.push(alki)
-// }
 
 function displayData() {
   for (let i = 0; i < locations.length; i++)
